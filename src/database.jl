9b1f5e583c77de77ecef36ef48a5c51259477bac
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-06-07 16:18:43 +0900

    Bind grn_db_create()

--- src/database.jl
+++ src/database.jl
@@ -1,3 +1,12 @@
+function db_create(context, path)
+  db = ccall((:grn_db_create, libgroonga),
+             Ptr{Void}, (Ptr{Void}, Ptr{UInt8},), context, path)
+  if db == C_NULL
+    error("Database open failed: $path")
+  end
+  db
+end
+
 function db_open(context, path)
   ccall((:grn_db_open, libgroonga),
         Ptr{Void}, (Ptr{Void}, Ptr{UInt8},), context, path)
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,14 @@ using Groonga
 using Base.Test
 
 Groonga.init()
+context = Groonga.ctx_open()
 
 @test Groonga.default_encoding() == "utf8"
+
+@test_throws ErrorException Groonga.db_create(context, "a/b/c/d/e")
+
+dir = mktempdir()
+database = Groonga.db_create(context, "$dir/test.db")
+println(Groonga.execute_command(context, "status"))
 
 Groonga.fin()
