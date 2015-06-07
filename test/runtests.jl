858e07ccfa16acad45c7a7cbcab8f31b741af833
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-06-07 17:29:07 +0900

    Add failing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,4 +16,10 @@ println(Groonga.execute_command(context, "status"))
 Groonga.execute_command(context, "table_create --name Site --flags TABLE_HASH_KEY --key_type ShortText")
 println(Groonga.execute_command(context, "select --table Site"))
 
+Groonga.execute_command(context, "column_create --table Site --name title --type ShortText")
+println(Groonga.execute_command(context, "select --table Site"))
+
+Groonga.execute_command(context, "load --table Site [{\"_key\":\"http://example.org/\",\"title\":\"This is test record 1!\"}, {\"_key\":\"http://example.net/\",\"title\":\"test record 2.\"}]")
+println(Groonga.execute_command(context, "select --table Site --query _id:1 --output_columns _key,title,_score"))
+
 Groonga.fin()
